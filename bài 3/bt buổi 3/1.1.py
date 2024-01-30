1622f94f15f42f7856f4c30741b217037eb78adb
Leo22hung <153985628+Leo22hung@users.noreply.github.com>
2024-01-30 09:46:11 +0700

    Add files via upload

---
+++ bài 3/bt buổi 3/1.1.py
@@ -0,0 +1,9 @@
+# Tạo một mảng
+my_array = [1, 2, 3, 4, 5]
+
+# Sử dụng list comprehension để tạo danh sách giá trị của từng phần tử
+result_list = [element for element in my_array]
+
+# In danh sách kết quả
+print("Danh sách giá trị:")
+print(result_list)
